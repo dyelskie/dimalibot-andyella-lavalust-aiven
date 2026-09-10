4392d645f06ed50efe6cb591684920d8075b413b
Andyella Dimalibot <andyellaud@gmail.com>
2026-09-10 11:01:43 +0800

    Add Authentication and Product CRUD

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://dimalibot-andyella-product-crud.onrender.comgit';
+$config['base_url'] 				= 'https://dimalibot-andyella-product-crud.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
